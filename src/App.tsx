444e0f4239ed23c956517b693bfdcf76d1f18413
Aleksey Zheryakov <az1773443@gmail.com>
2021-04-06 19:20:20 +0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
+export default function App() {
+  function getBeer() {
+    return axios.get('https://api.punkapi.com/v2/beers').then((response) => console.log(response));
+  }
+
+  getBeer();
+
+  function Home() {
+    return <h2>Home</h2>;
+  }
+
+  function Details() {
+    return <h2>Details</h2>;
+  }
+
+  function Сart() {
+    return <h2>Сart</h2>;
+  }
+
+  function Order() {
+    return <h2>Order</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Главная</Link>
+            </li>
+            <li>
+              <Link to="/details">Детальная информация</Link>
+            </li>
+            <li>
+              <Link to="/cart">Корзина</Link>
+            </li>
+            <li>
+              <Link to="/order">Оформление заказа</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/order">
+            <Order />
+          </Route>
+          <Route path="/details">
+            <Details />
+          </Route>
+          <Route path="/cart">
+            <Сart />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
